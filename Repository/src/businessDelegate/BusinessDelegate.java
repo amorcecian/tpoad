680cc1f87702ab7bfc1a35f3e26eea30e2dd5b10
aybjs <alexiscanclini@gmail.com>
2017-06-12 21:30:30 -0300

    actualice objeto remoto

--- Repository/src/businessDelegate/BusinessDelegate.java
+++ Repository/src/businessDelegate/BusinessDelegate.java
@@ -7,9 +7,11 @@ import java.rmi.Naming;import java.rmi.RemoteException;import java.util.List;
 import dto.ClienteDTO;import dto.EmpleadoDTO;import dto.MaterialDTO;import dto.SucursalDTO;import exceptions.ExceptionCliente;import interfaces.*;
 
 public class BusinessDelegate implements IControllerVentas {
+public class BusinessDelegate implements IController {
 	
 	private static BusinessDelegate instancia=null;
 	private IControllerVentas objetoRemoto;
+	private IController objetoRemoto;
 	
 	
 	public static BusinessDelegate getInstancia(){

f718538d83b9906bf5e435ef09b835b8d97b474c
amorcecian <amorcecian@gmail.com>
2017-07-03 00:36:28 -0300

    StockDAO

--- Server/src/dao/StockDAO.java
+++ Server/src/dao/StockDAO.java
@@ -1,9 +1,15 @@
 package dao;
 
-import org.hibernate.SessionFactory;
+import java.util.List;
+import java.util.Vector;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+
+import entities.PrendaVentaEntity;
 import entities.StockEntity;
 import hbt.HibernateUtil;
+import negocio.PrendaVenta;
 import negocio.Stock;
 
 public class StockDAO {
@@ -19,14 +25,49 @@ public class StockDAO {
 		return instancia;
 	}
 	
+	public void agregarStock(Stock stock){
+		Session s = sf.openSession();
+		s.beginTransaction().begin();
+		s.save(stock);
+		s.flush();
+		s.getTransaction().commit();
+		s.close();
+	}
+	
 	public void actualizarStock(Stock stock) {
-		// TODO Auto-generated method stub
+		Session s = sf.openSession();
+		StockEntity se = toEntity(stock);
+		s.beginTransaction().begin();
+		s.update(se);
+		s.flush();
+		s.beginTransaction().commit();
+		s.close();
 		
 	}
 
-	public Stock toNegocio(StockEntity stock) {
-		// TODO Auto-generated method stub
-		return null;
+	public Stock toNegocio(StockEntity se) {
+		Stock s = new Stock();
+		s.setIdStock(se.getIdStock());
+		s.setCantidad(se.getCantidad());
+		s.setActivo(se.isActivo());
+		List<PrendaVenta> prendasVenta = new Vector<PrendaVenta>();
+		for(PrendaVentaEntity i:se.getPrendasVenta())
+			prendasVenta.add(PrendaVentaDAO.getInstancia().toNegocio(i));
+		s.setPrendasVenta(prendasVenta);
+		return s;
+		
+	}
+	
+	public StockEntity toEntity(Stock s){
+		StockEntity se = new StockEntity();
+		se.setIdStock(s.getIdStock());
+		se.setCantidad(s.getCantidad());
+		s.setActivo(s.isActivo());
+		List<PrendaVentaEntity> prendasVenta = new Vector<PrendaVentaEntity>();
+		for(PrendaVenta i:s.getPrendasVenta())
+			prendasVenta.add(PrendaVentaDAO.getInstancia().PrendaVentaToEntity(i));
+		se.setPrendasVenta(prendasVenta);
+		return se;
 	}
 
 }

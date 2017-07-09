f57732b97f2e608aaf4c020883e2778b15b52fef
aybjs <alexiscanclini@gmail.com>
2017-07-09 20:13:23 -0300

    compra materiales terminado

--- Server/src/test/Test2.java
+++ Server/src/test/Test2.java
@@ -3,6 +3,7 @@ package test;
 import java.util.Calendar;
 import java.util.Vector;
 
+import controlador.ControladorCompra;
 import controlador.ControladorVenta;
 import dao.PrendaDAO;
 import dto.ItemPedidoDTO;
@@ -12,11 +13,9 @@ import negocio.ItemPedido;
 public class Test2 {
 
 	public static void main(String[] args) throws ExceptionCliente {
-		ItemPedido i = new ItemPedido(1, PrendaDAO.getInstance().obtenerPrenda(1), "Activo", true);
-		java.util.List<ItemPedidoDTO> items = new Vector<ItemPedidoDTO>();
-		items.add(i.toDTO());
-		ControladorVenta.getInstancia().generarPedido(items, Calendar.getInstance().getTime().toString(), 1,1, 1, "Activo");
-
+		ControladorCompra.getInstancia().generarOrdenCompra(prenda, orden);
+		
+		
 	}
 
 }

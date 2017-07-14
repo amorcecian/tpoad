9b377e7acc8897c21242ed2c9310a8ee0b11c094
amorcecian <amorcecian@gmail.com>
2017-07-14 09:12:24 -0300

    Agrego clase Transacciones

---
+++ Server/src/negocio/Transacciones.java
@@ -0,0 +1,22 @@
+package negocio;
+
+public class Transacciones {
+	
+	private String fecha;
+	private String cliente;
+	private String pedido;
+	private String empleado;
+	
+	
+	public Transacciones(String fecha, String cliente, String pedido,
+			String empleado) {
+		super();
+		this.fecha = fecha;
+		this.cliente = cliente;
+		this.pedido = pedido;
+		this.empleado = empleado;
+	}
+	
+	
+
+}

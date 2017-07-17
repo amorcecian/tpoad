a7190a7faf48c7da3c100d9a1e40c1dded3c31d9
amorcecian <amorcecian@gmail.com>
2017-07-17 12:03:39 -0300

    Pruebo ContinuarPRD

---
+++ Server/src/test/ContinuarProduccion.java
@@ -0,0 +1,12 @@
+package test;
+
+import controlador.ControladorProduccion;
+
+public class ContinuarProduccion {
+
+	public static void main(String[] args) {
+		ControladorProduccion.getInstancia().ContinuarProduccion(1);
+
+	}
+
+}
